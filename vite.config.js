6a7edcf917104a5f69a992ea408bf7b0e2ac54c2
Syed_Waseem <syedarw4794@gmail.com>
2024-01-31 19:17:14 +0530

    Deployement

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     server: {
         proxy: {
-<<<<<<< HEAD
-            '/api' : 'https://backend-blogify.onrender.com/'
-=======
-            '/api' : 'https://blogify-api-p7qp.onrender.com'
->>>>>>> ef759ca9ca4ba7f7f00edb7ff6d2972c78a92bcd
+            '/api' : 'https://backend-blogify.onrender.com/',
         }
     },
   plugins: [react()],
